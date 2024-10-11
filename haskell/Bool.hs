4ec6c5e8ea5f64ee33a071e62ccaf418242f6b85
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-10-11 17:24:32 -0300

    Implement And and Or operations for Bool

--- haskell/Bool.hs
+++ haskell/Bool.hs
@@ -8,3 +8,11 @@ data Bool where
   False :: Bool
   True :: Bool
   deriving (Show)
+
+band :: Bool -> Bool -> Bool
+band True True = True
+band _ _ = False
+
+bor :: Bool -> Bool -> Bool
+bor False False = False
+bor _ _ = True
