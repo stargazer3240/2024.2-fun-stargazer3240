4f9227ca5868eb42557f50f6f6f702fa785d0752
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-10-16 15:43:35 -0300

    Replace operations with suggared version

--- haskell/Nat.hs
+++ haskell/Nat.hs
@@ -1,18 +1,18 @@
 module Nat where
 
-import Prelude hiding (Num (..))
+import Prelude hiding (Num (..), (^))
 
 data Nat = O | S Nat
   deriving (Eq, Show)
 
-plus :: Nat -> Nat -> Nat
-plus n O = n
-plus n (S m) = S (plus n m)
+(+) :: Nat -> Nat -> Nat
+n + O = n
+n + (S m) = S (n + m)
 
-times :: Nat -> Nat -> Nat
-times n O = O
-times n (S m) = plus (times n m) n
+(*) :: Nat -> Nat -> Nat
+n * O = O
+n * (S m) = n * m + n
 
-power :: Nat -> Nat -> Nat
-power n O = S O
-power n (S m) = times (power n m) n
+(^) :: Nat -> Nat -> Nat
+n ^ O = S O
+n ^ (S m) = (n ^ m) * n
