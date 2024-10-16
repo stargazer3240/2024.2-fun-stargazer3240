d032f6e57f82b64829b0e04b4618f5ac4ab4c2cb
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-10-16 16:02:55 -0300

    Move Nat functions from hw1 to Nat file

--- haskell/Nat.hs
+++ haskell/Nat.hs
@@ -15,4 +15,27 @@ n * (S m) = n * m + n
 
 (^) :: Nat -> Nat -> Nat
 n ^ O = S O
-n ^ (S m) = (n ^ m) * n
+n ^ (S m) = n ^ m * n
+
+-- abbrevs
+o, so, sso, ssso :: Nat
+o = O
+so = S o
+sso = S so
+ssso = S sso
+
+double :: Nat -> Nat
+double n = n * sso
+
+fact :: Nat -> Nat
+fact O = S O
+fact (S n) = S n * fact n
+
+fib :: Nat -> Nat
+fib (S O) = S O
+fib (S (S O)) = S O
+fib (S (S n)) = fib (S n) + fib n
+
+pred :: Nat -> Nat
+pred (S n) = n
+pred _ = error "0 doesn't have pred!"
